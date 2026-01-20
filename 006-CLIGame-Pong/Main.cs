4d6d09223df5fc7ef756ea0ec3c9d8b45bf3c230
TU-PJY <viska0319@icloud.com>
2026-01-20 16:35:29 +0900

    added player and player control

--- 006-CLIGame-Pong/Main.cs
+++ 006-CLIGame-Pong/Main.cs
@@ -16,6 +16,10 @@ namespace Pong {
         // ball
         public static Ball ball;
 
+        // bar
+        public static Player p1;
+        public static Player p2;
+
         // if true program end
         static bool forceExit = false;
 
@@ -55,11 +59,22 @@ namespace Pong {
                 outline = new(SPACE_WIDTH, SPACE_HEIGHT);
                 outline.Render();
 
+                // perpare players
+                p1 = new(1, SPACE_HEIGHT / 2, 7);
+                p2 = new(outline.right, SPACE_HEIGHT / 2, 7);
+
                 // exit the game if esc input
                 while (true) {
-                    var input = Console.ReadKey();
+                    var input = Console.ReadKey(true);
                     if (input.Key == ConsoleKey.Escape)
                         break;
+
+                    // key input to players
+                    if (input.Key == ConsoleKey.A || input.Key == ConsoleKey.Z)
+                        p1.InputKey(input.Key);
+
+                    if (input.Key == ConsoleKey.DownArrow || input.Key == ConsoleKey.UpArrow)
+                        p2.InputKey(input.Key);
                 }
 
                 // stop programm
@@ -74,7 +89,7 @@ namespace Pong {
         }
 
         private static void SwapBuffers() {
-            string blankLine = new string(' ', SPACE_WIDTH * 2 + 1);
+            string blankLine = new string(' ', SPACE_WIDTH * 2 + 2);
             for (int i = 0; i < SPACE_HEIGHT; i++) {
                 Console.SetCursorPosition(1, 1 + i);
                 Console.WriteLine(blankLine);
@@ -87,6 +102,8 @@ namespace Pong {
 
             SwapBuffers();
             ball.Render();
+            p1.Render();
+            p2.Render();
         }
     }
 }
---
+++ 006-CLIGame-Pong/Player.cs
@@ -0,0 +1,34 @@
+namespace Pong {
+    internal class Player {
+        private int length;
+        private int x, y;
+        public int left, right;
+        public int top, bottom;
+
+        public Player(int x, int y, int length) {
+            this.x = x;
+            this.y = y;
+            this.length = length;
+            top = y - (length / 2);
+            bottom = y + (length / 2);
+        }
+
+        public void Render() {
+            Console.BackgroundColor = ConsoleColor.White;
+            for (int i = 0; i < length; i++) {
+                Console.SetCursorPosition(x, y - (length / 2) + i);
+                Console.WriteLine("  ");
+            }
+            Console.ResetColor();
+        }
+
+        public void InputKey(ConsoleKey input) {
+            if (input == ConsoleKey.DownArrow || input == ConsoleKey.Z)
+                y += 1;
+            else if (input == ConsoleKey.UpArrow || input == ConsoleKey.A)
+                y -= 1;
+
+            y = Math.Clamp(y, MainLoop.outline.top + length / 2, MainLoop.outline.bottom - length / 2);
+        }
+    }
+}
