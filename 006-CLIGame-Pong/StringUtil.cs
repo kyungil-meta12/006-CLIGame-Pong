a32877a5317de8e73c95946c0add57465ebebfa1
TU-PJY <viska0319@icloud.com>
2026-01-21 10:51:39 +0900

    removed unused import

--- 006-CLIGame-Pong/StringUtil.cs
+++ 006-CLIGame-Pong/StringUtil.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pong {
+﻿namespace Pong {
     public class StringUtil {
         // renders string at position with middle aligned
         public void RenderStringMiddleAligned(int x, int y, string str) {
